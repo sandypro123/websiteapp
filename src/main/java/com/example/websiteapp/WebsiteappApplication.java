d691cf6da3f617a733d2d743b507e7b440920ca9
sandy <2399934250@qq.com>
2024-10-14 20:57:09 +0800

    init project

---
+++ src/main/java/com/example/websiteapp/WebsiteappApplication.java
@@ -0,0 +1,13 @@
+package com.example.websiteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websiteapp/WebsiteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websiteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
